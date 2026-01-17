76c94f0e496c2f2ce1b952da00e661e05372a2ff
John Mburu <111112361+mburuxx@users.noreply.github.com>
2026-01-17 14:17:41 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,11 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+]from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("users.urls")),
 ]
