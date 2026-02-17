7a03bea71f671a5bf417cc1eb3bba170ad1e9c25
mburuxx <njihiamburu.3@gmail.com>
2026-02-17 21:51:05 +0300

    configure for railway deployment

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS += [".railway.app"]
 
 # Application definition
 
